9621f2cd82486ca37da5d93c6471a9d5e1ae53dc
Rahat2003RT <rturmyshov@gmail.com>
2026-06-22 17:02:00 +0500

    Добавил настроику Scramble

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,16 @@ class AppServiceProvider extends ServiceProvider
         });
         Gate::define('access-elite', function (User $user) {
             return $user->isElite();
+        });
+
+        Scramble::auth(function ($request) {
+            if (app()->environment('local')) {
+                return true;
+            }
+            if (config('app.env') === 'production' && env('ALLOW_API_DOCS') === true) {
+                return true;
+            }
+            return false;
         });
     }
 }
