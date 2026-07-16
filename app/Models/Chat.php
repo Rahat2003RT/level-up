7923f8f07dd9faa72cfae8b713aca8d409c8c709
Rahat2003RT <rturmyshov@gmail.com>
2026-07-16 16:16:12 +0500

    Правка

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -41,6 +42,8 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  */
 final class Chat extends Model
 {
+
+    use HasFactory;
     protected $fillable = [
         'elite_id',
         'leader_id',
