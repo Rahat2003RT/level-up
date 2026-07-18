540d9e4933a1d4f282179536e44dad733bad9479
Rahat2003RT <rturmyshov@gmail.com>
2026-07-18 16:33:06 +0500

    Команда автоудаления пользователей

--- routes/console.php
+++ routes/console.php
@@ -7,3 +7,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 Schedule::command('checklists:auto-close')->hourly();
+Schedule::command('users:prune-deleted')->daily();
